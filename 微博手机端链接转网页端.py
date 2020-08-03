5c070464d5c1312dbcfc48b183b78bcd8a7ce6b6
dennis33560 <53635916+dennis33560@users.noreply.github.com>
2020-08-03 16:37:53 +0800

    Update 微博手机端链接转网页端.py

--- 微博手机端链接转网页端.py
+++ 微博手机端链接转网页端.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
 
 import pandas as pd 
 import requests
 import re
 
+#用pandas包里专门读取excel下的各类文档类型的功能读取文档，并把‘链接’转为list数据类型
 df1=pd.read_excel("weibo.xlsx")
 urllist=df1.链接.tolist()
 
+#创建变量
 list=[]
 
+#循环次数为行数长度
 for i in urllist:
     res = requests.get(i)
-    uid=re.search(r'"uid": (\d+)',res.text).group(1)
-    bid=re.search(r'"bid": "([A-Za-z0-9]+)',res.text).group(1)
-    url="https://weibo.com/"+uid+"/"+bid
-    list.append(url)
+    uid=re.search(r'"uid": (\d+)',res.text).group(1) #获得uid
+    bid=re.search(r'"bid": "([A-Za-z0-9]+)',res.text).group(1) #获得bid
+    url="https://weibo.com/"+uid+"/"+bid #创建网页端格式的链接
     
+    #储存到list的变量里
+    list.append(url)
+
+#输出保存为"hehe.csv"同时不带有列抬头
 pd.DataFrame(list).to_csv("hehe.csv",header=None,index=False)
 
